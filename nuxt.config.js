9e62e363fa1035d8f5db1474014d068f1ab8323f
clomie <cclomie@gmail.com>
2020-09-26 19:21:36 +0900

    Add hooks to restore createdAt and updatedAt from commit timestamp

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,5 @@
+import { execSync } from 'child_process'
+
 export default {
   // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
@@ -38,4 +40,17 @@ export default {
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {},
+
+  hooks: {
+    'content:file:beforeInsert': (document, database) => {
+      const path = document.path.slice(1) + document.extension
+      const cmd = `git --no-pager log --no-color --pretty=format:'%ad' -- ${path}`
+      const output = execSync(cmd, { cwd: database.dir }).toString()
+      if (output) {
+        const timestamps = output.split('\n')
+        document.updatedAt = new Date(timestamps[0])
+        document.createdAt = new Date(timestamps.pop())
+      }
+    },
+  },
 }
